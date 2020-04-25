0986c5a9fdfb6429b122bc697449a207f8159c44
Vivek Singh <Vivekanand_Singh@dell.com>
2020-04-25 17:30:41 +0530

    Initial Commit

---
+++ TinkLibraryJniForNet/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace TinkLibraryJniForNet
+{
+    class Program
+    {
+        private const string _paymentSampleToken = "{\"signature\":\"MEUCIQCovQ0tGptXIXWG2Dmz4Rj3nJ0L2c9lj9j9CC5yGvv0BAIgTi9s1Qgs6GrIe5bpI/emThUuu5keOcVxwWyx8eNXLYU\\u003d\",\"intermediateSigningKey\":{\"signedKey\":\"{\\\"keyValue\\\":\\\"MFkwEwYHKoZIzj0CAQYIKoZIzj0DAQcDQgAEKyoSUKDpyCbf0xB484CC+iXHzmNQ/qOttFKlePfuOo0QT7neC9O0pROUHGnDougKpR/TMjQjYbzE0yg1SgRdJw\\\\u003d\\\\u003d\\\",\\\"keyExpiration\\\":\\\"1588362031469\\\"}\",\"signatures\":[\"MEUCIEePwtLdWxjKh6LeMLh/Nvi1yYAh+poJm9M7VKwdomQTAiEA1+Hf8JOHOyrtKlaaRtNxWI7zVt9eQQMZUspoaO2Zcks\\u003d\"]},\"protocolVersion\":\"ECv2\",\"signedMessage\":\"{\\\"encryptedMessage\\\":\\\"3Q++43GmAn+8OXqYn8UU6M4rZT0IjlSqDlGabPMg+s5Z1YkM3xEjR3VS51rFQmuhnprHe4LJPTptwQhlQGKSJhZwsvBktPzNXm3qoP2JHwYQwAWMLxm+kk+qxxa6mf6e89Vcts3fbPDht0B3wEh2kHqYjYt9DF5s5hwRGxNDBo5RYn0Xyh1exOpoNoJwQurppgsc3Nq3ijuQQFlB3GBWDwdlCcZE2zJNzkvvXpZ6MOwMszmRpFX7cMduaJnYJuzmajAGiZ5h3GVyfkGc0QWj1ayutwQh8A84NA/89n4uMkFicuCiw5Mi6QTpxFeT/C1xs0P89MBkI44HrP1KMMXdgay564I36ZKgGr/HQm45IGpeLUn2y+EsqWBhmJyQUTA+sgqaQL8Xq23f2FYh9lzZ7jB/nQIryHQ0yACpv2P08gBLCFSltmKEeK4AIM0\\\\u003d\\\",\\\"ephemeralPublicKey\\\":\\\"BBv1CAD8F1m7sPE3PHv87p5pq2Osmw2c+UB8q1YbCJrCQPVRSdnkYnYOq9/tULyfM7uyHODz3MgzC1lpUU6m6Ko\\\\u003d\\\",\\\"tag\\\":\\\"s59nweJsTXwV1qSs5nYpT0B2OStHNOtVeTNcdTrQQ4c\\\\u003d\\\"}\"}";
+        static void Main(string[] args)
+        {
+            var decryptor = new TinkDecryptor();
+            // print out payment sample text
+            Console.Write("Sample Token: ");
+            Console.WriteLine(_paymentSampleToken);
+            Console.WriteLine();
+            // decrypt and print out plain text
+            Console.Write("Plain Text: ");
+            Console.WriteLine(decryptor.Decrypt(_paymentSampleToken));
+            Console.ReadLine();
+        }
+    }
+}
---
+++ TinkLibraryJniForNet/TinkDecryptor.cs
@@ -0,0 +1,77 @@
+﻿using java.lang;
+using net.sf.jni4net;
+using net.sf.jni4net.jni;
+using System.Configuration;
+using Sys = System;
+
+namespace TinkLibraryJniForNet
+{
+    public class TinkDecryptor
+    {
+        private JNIEnv _jniEnv = null;
+        private string _trustedSigningKeysJson = null;
+
+        /// <summary>
+        /// Create TinkDecryptor by creating JNI .Net bridge and preload trusted signing keys from Google servers
+        /// </summary>
+        public TinkDecryptor()
+        {
+            // create bridge using jni4net.j.jar in the same folder as jni4net.n.dll
+            var bridgeSetup = new BridgeSetup();
+            bridgeSetup.AddAllJarsClassPath("./tink_jars/"); // load libs
+            _jniEnv = Bridge.CreateJVM(bridgeSetup); // create jvm
+
+            // preload trusted signing keys from Google servers, cache into memory before performing any transactions
+            Class googlePaymentsPublicKeysManager = _jniEnv.FindClass("com/google/crypto/tink/apps/paymentmethodtoken/GooglePaymentsPublicKeysManager");
+            var env = ConfigurationManager.AppSettings["Trusted_Signing_Keys_Env"]; // environment of trusted signing keys from configs
+            if (env != "INSTANCE_TEST" && env != "INSTANCE_PRODUCTION")
+                throw new ConfigurationErrorsException("Setting Trusted_Signing_Keys_Env must be either INSTANCE_TEST or INSTANCE_PRODUCTION.");
+
+            // initialize public key manager and load signing keys
+            var publicKeyManager = googlePaymentsPublicKeysManager.GetFieldValue<Object>(env, "Lcom/google/crypto/tink/apps/paymentmethodtoken/GooglePaymentsPublicKeysManager;");
+            publicKeyManager.Invoke("refreshInBackground", "()V");
+            _trustedSigningKeysJson = publicKeyManager.Invoke<String>("getTrustedSigningKeysJson", "()Ljava/lang/String;");
+        }
+
+        /// <summary>
+        /// Decrypt the given cipher text by performing the necessary signature verification and * decryption (if required) steps based on the protocolVersion
+        /// </summary>
+        /// <param name="cipherText">cipher text</param>
+        /// <returns>plain text</returns>
+        public string Decrypt(string cipherText)
+        {
+            try
+            {
+                // load configs
+                var recipientId = ConfigurationManager.AppSettings["Recipient_Id"];
+                if (string.IsNullOrWhiteSpace(recipientId))
+                    throw new ConfigurationErrorsException("Setting Recipient_Id must be set.");
+                var privateKey = ConfigurationManager.AppSettings["Base64_PKCS8_Private_key"];
+                if (string.IsNullOrWhiteSpace(privateKey))
+                    throw new ConfigurationErrorsException("Setting Base64_PKCS8_Private_key must be set.");
+
+                // build payment method token recipient
+                Class paymentMethodTokenRecipientBuilder = _jniEnv.FindClass("com/google/crypto/tink/apps/paymentmethodtoken/PaymentMethodTokenRecipient$Builder");
+                var recipientBuilder = paymentMethodTokenRecipientBuilder.newInstance();
+                recipientBuilder.Invoke<Object>("senderVerifyingKeys",
+                    "(Ljava/lang/String;)Lcom/google/crypto/tink/apps/paymentmethodtoken/PaymentMethodTokenRecipient$Builder;", _trustedSigningKeysJson);
+                recipientBuilder.Invoke<Object>("recipientId", "(Ljava/lang/String;)Lcom/google/crypto/tink/apps/paymentmethodtoken/PaymentMethodTokenRecipient$Builder;",
+                   recipientId);
+                recipientBuilder.Invoke<Object>("protocolVersion", "(Ljava/lang/String;)Lcom/google/crypto/tink/apps/paymentmethodtoken/PaymentMethodTokenRecipient$Builder;",
+                    "ECv2");
+                recipientBuilder.Invoke<Object>("addRecipientPrivateKey", "(Ljava/lang/String;)Lcom/google/crypto/tink/apps/paymentmethodtoken/PaymentMethodTokenRecipient$Builder;",
+                    privateKey);
+                var recipient = recipientBuilder.Invoke<Object>("build", "()Lcom/google/crypto/tink/apps/paymentmethodtoken/PaymentMethodTokenRecipient;");
+
+                // decrypt message
+                var plainText = recipient.Invoke<String>("unseal", "(Ljava/lang/String;)Ljava/lang/String;", cipherText);
+                return plainText;
+            }
+            catch (Sys.Exception ex)
+            {
+                throw ex;
+            }
+
+        }
+    }
+}
